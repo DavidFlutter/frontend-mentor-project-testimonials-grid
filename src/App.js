e6799ab25f3589f65f25cc689434bc2f1e092c79
DavidFlutter <david.haastrup@stu.cu.edu.ng>
2023-10-06 17:52:22 +0000

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
+import data from "./data/data";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="tile1">
+          <div className="upper">
+            <img src={data[0].image} alt="" />
+            <div className="text">
+              <span>{data[0].name}</span>
+              <span>{data[0].status}</span>
+            </div>
+          </div>
+          <div className="content">
+            <div className="title">
+              {data[0].title}
+            </div>
+            <div className="story">
+              {data[0].content}
+            </div>
+          </div>
+        </div>
+
+        <div className="tile2">
+          <div className="upper">
+            <img src={data[1].image} alt="" />
+            <div className="text">
+              <span>{data[1].name}</span>
+              <span>{data[1].status}</span>
+            </div>
+          </div>
+          <div className="content">
+            <div className="title">
+              {data[1].title}
+            </div>
+            <div className="story">
+              {data[1].content}
+            </div>
+          </div>
+        </div>
+
+        <div className="tile3">
+          <div className="upper">
+            <img src={data[2].image} alt="" />
+            <div className="text">
+              <span>{data[2].name}</span>
+              <span  classname="first">{data[2].status}</span>
+            </div>
+          </div>
+          <div className="content">
+            <div className="title">
+              {data[2].title}
+            </div>
+            <div className="story">
+              {data[2].content}
+            </div>
+          </div>
+        </div>
+
+        <div className="tile4">
+          <div className="upper">
+            <img src={data[3].image} alt="" />
+            <div className="text">
+              <span>{data[3].name}</span>
+              <span classname="first">{data[3].status}</span>
+            </div>
+          </div>
+          <div className="content">
+            <div className="title">
+              {data[3].title}
+            </div>
+            <div className="story">
+              {data[3].content}
+            </div>
+          </div>
+        </div>
+
+        <div className="tile5">
+          <div className="upper">
+            <img src={data[4].image} alt="" />
+            <div className="text">
+              <span>{data[4].name}</span>
+              <span classname="first">{data[4].status}</span>
+            </div>
+          </div>
+          <div className="content">
+            <div className="title">
+              {data[4].title}
+            </div>
+            <div className="story">
+              {data[4].content}
+            </div>
+          </div>
+        </div>
+
+
+      </div>
     </div>
   );
 }
